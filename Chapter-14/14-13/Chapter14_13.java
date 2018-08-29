3ffb8238ca57ba5894072c5e52eb2aa511d393d4
drakerhunt <Drakerhunt44@gmail.com>
2018-08-29 14:50:27 -0600

    Created assignment 14-13

---
+++ Chapter-14/14-13/Chapter14_13.java
@@ -0,0 +1,9 @@
+public class Chapter14_13 extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+}
