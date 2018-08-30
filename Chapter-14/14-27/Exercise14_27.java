f16a8f172ed8a3e6831c14ec2124ea539e0eab69
Drake <Drakerhunt44@gmail.com>
2018-08-30 10:25:32 -0600

    Added ClockPane Class

--- Chapter-14/14-27/Exercise14_27.java
+++ Chapter-14/14-27/Exercise14_27.java
@@ -6,14 +6,23 @@ import javafx.scene.shape.Circle;
 import javafx.scene.text.*;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
+import javafx.geometry.*;
 
-class Exercise14_27 extends Application {
+public class Exercise14_27 extends Application {
 	@Override
 	public void start(Stage primaryStage) {
+		ClockPane clock = new ClockPane().setCurrentTime();
 		
+		Pane pane = new Pane();
+		pane.setCenter(clock);
+		
+		Scene scene = new Scene(pane, 300, 300);
+		primaryStage.setTitle("14-27");
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 	
 	public static void main(String[] args) {
-		Application.lauch(args);
+		Application.launch(args);
 	}
 }
