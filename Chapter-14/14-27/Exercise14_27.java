d2b51ecc674e260c4962faf27f9805ac2c0a3185
Drake <Drakerhunt44@gmail.com>
2018-08-30 09:48:40 -0600

    Started Exercise 14-27

---
+++ Chapter-14/14-27/Exercise14_27.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.*;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+class Exercise14_27 extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+	
+	public static void main(String[] args) {
+		Application.lauch(args);
+	}
+}
